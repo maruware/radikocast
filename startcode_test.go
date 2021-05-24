31b528db258c028110d6cd794c0bcfd5c44c5fe0
maruware <me@maruware.com>
2021-05-25 01:57:14 +0900

    Fix TestStartCode

--- startcode_test.go
+++ startcode_test.go
@@ -9,7 +9,7 @@ func TestStartCode(t *testing.T) {
 	baseTime := time.Date(2010, 2, 3, 2, 5, 0, 0, time.UTC) // wednesday
 
 	day := "weekday"
-	at := "01:15-02:00"
+	at := "01:15"
 	expect := "20100203011500"
 
 	s, err := findLastProgram(day, at, baseTime)
@@ -23,7 +23,7 @@ func TestStartCode(t *testing.T) {
 
 	baseTime = time.Date(2010, 2, 7, 3, 5, 0, 0, time.UTC) //sunday
 	day = "saturday"
-	at = "25:00-27:00"
+	at = "25:00"
 	expect = "20100207010000"
 
 	s, err = findLastProgram(day, at, baseTime)
